6d4af1966376ff3abd7c5a7d2d53a1cc18296930
Nathan Standage <nathanstandage@gmail.com>
2017-10-07 16:24:50 -0600

    initial commit

---
+++ Slot Machine/SlotMachine.swift
@@ -0,0 +1,51 @@
+//
+//  SlotMachineProvider.swift
+//  Slot Machine
+//
+//  Created by Nathan Standage on 9/21/17.
+//  Copyright © 2017 Nathan Standage. All rights reserved.
+//
+
+import UIKit
+
+
+
+struct SlotMachine {
+    let slotArray = ["🦊","🐸","🌟","🐬"]
+    var winner = false
+    var match = false
+    
+    
+    
+    func randomNumber() -> Int {
+        return Int(arc4random_uniform(UInt32(slotArray.count)))
+        
+        
+    }
+    mutating func slotPull() -> [String] {
+        let array = [
+        slotArray[randomNumber()],
+        slotArray[randomNumber()],
+        slotArray[randomNumber()]
+        ]
+        
+        if array[0] == "🌟" && array[1] == "🌟" && array[2] == "🌟" {
+            
+            self.winner = true
+            
+        } else if array[0] == array[1] && array[0] == array[2] {
+            
+            self.match = true
+        }
+        
+        
+        
+        return array
+    }
+
+        
+}
+
+
+
+
---
+++ Slot Machine/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by Nathan Standage on 9/21/17.
+//  Copyright © 2017 Nathan Standage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var winnerOfNotLabel: UILabel!
+    
+    @IBOutlet weak var slotOne: UILabel!
+    @IBOutlet weak var slotTwo: UILabel!
+    @IBOutlet weak var slotThree: UILabel!
+    
+    var slot = SlotMachine()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.win
+        winnerOfNotLabel.text = ""
+        slotOne.text = ""
+        slotTwo.text = ""
+        slotThree.text = ""
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pullButtonPressed(_ sender: Any) {
+        //resetting
+        slot.match = false
+        slot.winner = false
+        winnerOfNotLabel.text = ""
+        
+       // displaying text
+        let pull = slot.slotPull()
+    
+        if pull.count == 3 {
+            slotOne.text = pull[0]
+            slotTwo.text = pull[1]
+            slotThree.text = pull[2]
+        }
+        
+        
+        //Winner or not?
+        if slot.winner == true {
+            winnerOfNotLabel.text = "JACKPOT!"
+            
+        } else if slot.match == true {
+            winnerOfNotLabel.text = "WE HAVE A MATCH!"
+        }
+        
+        
+     
+    }
+    
+}
+
